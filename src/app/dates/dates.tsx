c06c8ac34e939cbca73a62f203d3ba7757cd448c
GiftSibiya <giftsibiya.gb@gmail.com>
2024-03-15 16:01:10 +0200

    Dates Import

---
+++ src/app/dates/dates.tsx
@@ -0,0 +1,38 @@
+/// These are the date estimations of the death, burrial and ressurection of Jesus.
+// These are just sestimates i could get from the net so i know very well they are not accurate fam.
+
+/// Date Variables
+// dates are in the format:( YYYY,MM,DD, HH, MM, SS, MS)
+const PasssoverDate = new Date(2024, 2, 28, 18, 0, 0, 0); // Thursday 6pm
+const LastSupperDate = new Date(2024, 2, 28, 6, 0, 0, 0); // Friday 6am
+const GetsemanePrayerDate = new Date(2024, 2, 28, 0, 0, 0, 0); // Thursday 9am
+const JesusBetrayalDate = new Date(2024, 2, 29, 0, 0, 0, 0); // Friday 12am
+const HebrewTrail = new Date(2024, 2, 29, 4, 0, 0, 0); // Friday 4am
+const PilateTrial = new Date(2024, 2, 29, 7, 14, 0, 0); // Friday 7am
+const JesusTorture = new Date(2024, 2, 29, 8, 0, 0, 0); // Friday 3am
+const CarryCrossDate = new Date(2024, 2, 29, 6, 0, 0, 0); // Friday 6am
+const CrucifiedDate = new Date(2024, 2, 29, 9, 0, 0, 0); // Friday 9am
+const JesusDeathDate = new Date(2024, 2, 29, 15, 0, 0, 0); // Friday 3pm
+const BurialDate = new Date(2024, 2, 28, 29, 18, 0, 0); // Friday 6pm
+const RessurectionDate = new Date(2024, 2, 31, 18, 0, 0, 0); // Saturday 6pm
+const EnterTombDate = new Date(2024, 2, 31, 6, 0, 0, 0); // Sunday 6am
+// Jesus Ascends
+///---------------------
+
+// Exports
+const datesExports = {
+  LastSupperDate,
+  PasssoverDate,
+  GetsemanePrayerDate,
+  JesusBetrayalDate,
+  HebrewTrail,
+  PilateTrial,
+  JesusTorture,
+  CarryCrossDate,
+  CrucifiedDate,
+  JesusDeathDate,
+  BurialDate,
+  RessurectionDate,
+  EnterTombDate,
+};
+export default datesExports;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,50 @@
 "use client";
 import React, { useState, useEffect } from "react";
+import datesExports from "./dates/dates";
+
+// Custom hook for countdown timer
+function useCountdownTimer(targetDate: Date) {
+  const [countdown, setCountdown] = useState({
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+    days: 0,
+  });
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const now = new Date();
+      const msUntilTargetDate = targetDate.getTime() - now.getTime();
+      const seconds = Math.floor((msUntilTargetDate / 1000) % 60);
+      const minutes = Math.floor((msUntilTargetDate / 1000 / 60) % 60);
+      const hours = Math.floor((msUntilTargetDate / (1000 * 60 * 60)) % 24);
+      const days = Math.floor(msUntilTargetDate / (1000 * 60 * 60 * 24));
+
+      setCountdown({ days, hours, minutes, seconds });
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [targetDate]);
+
+  return countdown;
+}
 
 export default function Home() {
-  /// Dates
+  // Dates
+  const PassoverDate = new Date(datesExports.PasssoverDate);
+  const LastSupperDate = new Date(datesExports.LastSupperDate);
+  const GetsemanePrayerDate = new Date(datesExports.GetsemanePrayerDate);
+  const JesusBetrayalDate = new Date(datesExports.BurialDate);
+  const HebrewTrailDate = new Date(datesExports.HebrewTrail);
 
-  // dates are in the format:--- ( YYYY,MM,DD, HH, MM, SS)
-  const LastSupperDate = new Date(2024, 2, 28, 18, 0, 0, 0); // Wednesday
-  const PasssoverDate = new Date(2024, 2, 29, 13, 0, 0, 0); // Thursday
+  // Hooks for countdown timers
+  const passoverCountdown = useCountdownTimer(PassoverDate);
+  const lastSupperCountdown = useCountdownTimer(LastSupperDate);
+  const getsemaneCountdown = useCountdownTimer(GetsemanePrayerDate);
+  const betrayalCountdown = useCountdownTimer(JesusBetrayalDate);
+  const hebrewTrialCountdown = useCountdownTimer(HebrewTrailDate);
 
-  // Getsemane prayer
-  // Betrayed and Arrest by soldiers
-  // Trial taken to Annas and Caiaphas 4-6AM
-  // Peter denies Jesus
-  // Jesus trial Before Pilate
-  // Barabbas is frees
-  // Jesus Tortured
-  // Jesus Carries Cross
-  // Jesus Cruciffied 9pm
-  // Jesus Mocked and Soldiers mock him
-  // Jesus Speask to theif and John
-  // Darkness over the land
-  // Jesus Dies 3PM
-  // Soldiers pierce him 3-6pm
-  // Joseph claims his body
-  // Jesus tomb sealed
-  // Jesus Ressurects Saturday sunset
-  // Jesus Ascends
-
-  // Hooks
-  // Current Day
+  // Current time
   const [time, setTime] = useState(
     new Date().toLocaleTimeString("en-US", {
       hour: "numeric",
@@ -38,32 +54,8 @@ export default function Home() {
     })
   );
 
-  /// Countdowns
-
-  // Passover Countdown
-  const [passoverCountdown, setPassoverCountdown] = useState({
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-    days: 0,
-  });
-
-  // Last Supper Countdown
-  const [lastSupperCountdown, setLastSupperCountdown] = useState({
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-    days: 0,
-  });
-
-  ///---
-
-  // Functions
-
-  // Returns the time after every second.
   useEffect(() => {
     const intervalId = setInterval(() => {
-      const countdownTime = getTimeUntilPassoverDate();
       setTime(
         new Date().toLocaleTimeString("en-US", {
           hour: "numeric",
@@ -72,56 +64,12 @@ export default function Home() {
           hour12: false,
         })
       );
-      setPassoverCountdown(countdownTime);
     }, 1000);
 
     return () => clearInterval(intervalId);
   }, []);
 
-  // Returns the time of last supper in 1 sec interval.
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      const countdownTime = getTimeUntilLastSupper();
-      setTime(
-        new Date().toLocaleTimeString("en-US", {
-          hour: "numeric",
-          minute: "numeric",
-          second: "numeric",
-          hour12: false,
-        })
-      );
-      setLastSupperCountdown(countdownTime);
-    }, 1000);
-
-    return () => clearInterval(intervalId);
-  }, []);
-
-  // Time until Passover Calculation
-  function getTimeUntilPassoverDate() {
-    const targetDate = new Date(PasssoverDate);
-    const now = new Date();
-    const msUntilTargetDate = targetDate.getTime() - now.getTime();
-    const seconds = Math.floor((msUntilTargetDate / 1000) % 60);
-    const minutes = Math.floor((msUntilTargetDate / 1000 / 60) % 60);
-    const hours = Math.floor((msUntilTargetDate / (1000 * 60 * 60)) % 24);
-    const days = Math.floor(msUntilTargetDate / (1000 * 60 * 60 * 24));
-
-    return { days, hours, minutes, seconds };
-  }
-
-  //
-  function getTimeUntilLastSupper() {
-    const targetDate = new Date(LastSupperDate);
-    const now = new Date();
-    const msUntilTargetDate = targetDate.getTime() - now.getTime();
-    const seconds = Math.floor((msUntilTargetDate / 1000) % 60);
-    const minutes = Math.floor((msUntilTargetDate / 1000 / 60) % 60);
-    const hours = Math.floor((msUntilTargetDate / (1000 * 60 * 60)) % 24);
-    const days = Math.floor(msUntilTargetDate / (1000 * 60 * 60 * 24));
-
-    return { days, hours, minutes, seconds };
-  }
-
+  // JSX
   return (
     <main>
       <div className="">Current time is: {time}</div>
@@ -134,6 +82,21 @@ export default function Home() {
         Time until Last Supper is: {lastSupperCountdown.days} days,{" "}
         {lastSupperCountdown.hours} hours, {lastSupperCountdown.minutes}{" "}
         minutes, {lastSupperCountdown.seconds} seconds
+      </div>
+      <div className="">
+        Time until Getsemane is: {getsemaneCountdown.days} days,{" "}
+        {getsemaneCountdown.hours} hours, {getsemaneCountdown.minutes} minutes,{" "}
+        {getsemaneCountdown.seconds} seconds
+      </div>
+      <div className="">
+        Time until Jesus is betrayed is: {betrayalCountdown.days} days,{" "}
+        {betrayalCountdown.hours} hours, {betrayalCountdown.minutes} minutes,{" "}
+        {betrayalCountdown.seconds} seconds
+      </div>
+      <div className="">
+        Time until Hebrew Trial is: {hebrewTrialCountdown.days} days,{" "}
+        {hebrewTrialCountdown.hours} hours, {hebrewTrialCountdown.minutes}{" "}
+        minutes, {hebrewTrialCountdown.seconds} seconds
       </div>
     </main>
   );
